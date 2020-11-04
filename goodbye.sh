a4537b9f889ffb03b3d5a7337c5811c6454b966c
rain <34671786+lvyinggithub@users.noreply.github.com>
2020-11-04 17:34:14 +0800

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -1,6 +1,6 @@
 echo "Hello $1"
 echo "Goodbye"
-time=$(date)
-teststr="teststr"
-echo "::set-output name=time::$time"
-echo "::set-output name=teststr::$teststr"
+#time=$(date)
+#teststr="teststr"
+#echo "::set-output name=time::$time"
+#echo "::set-output name=teststr::$teststr"
